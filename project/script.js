4a920d2c3596a8be57f6f41a66aa63b74540d13f
shoaibhasann <shoaibhasan0940@gmail.com>
2023-07-25 20:49:51 +0530

    Create script file

---
+++ project/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * Mobile navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+const barIcon = document.querySelector("#bars-icon");
+
+navToggler.addEventListener("click", function () {
+  barIcon.classList.toggle("fa-xmark")  
+  navbar.classList.toggle("active");
+});
+
+/**
+ * Header active
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  header.classList[this.scrollY > 50 ? "add" : "remove"]("active");
+});
